cda0afbd4cb8380cc0b5c37db7af47158d1d4778
Wrzlprmft <Wrzlprmft@users.noreply.github.com>
2020-12-11 15:10:42 +0100

    Better setup of past for JiTCDDE

--- DDEs_models_test/divergingJumpsDDE.py
+++ DDEs_models_test/divergingJumpsDDE.py
@@ -47,7 +47,15 @@ DDE = jitcdde(f,
 		max_delay=20 # for plotting; lest history is forgotten
 	)
 DDE.set_integration_parameters(atol=atol,rtol=rtol)
-DDE.past_from_function(h,times_of_interest=np.linspace(-1.1,-1e-8,10))
+
+ε = 1e-8
+DDE.add_past_point(-1,1,0)
+for i,x in enumerate(np.arange(-0.8,-0.2,0.2)):
+	DDE.add_past_point(x-ε,(-1)** i   ,0)
+	DDE.add_past_point(x+ε,(-1)**(i+1),0)
+DDE.add_past_point(-ε,1,0)
+DDE.add_past_point( 0,1,1)
+DDE.initial_discontinuities_handled = True
 
 DDE.adjust_diff()
 data = []
